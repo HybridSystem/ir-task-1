573a2a6694d2bdb03c0a13e3baa87c7e4cb41d98
Taylor <e0725922@student.tuwien.ac.at>
2014-03-30 17:05:09 +0200

    Added basic argument handling

--- src/main/java/com/tuwien/isis/irtask1/RankingEngine.java
+++ src/main/java/com/tuwien/isis/irtask1/RankingEngine.java
@@ -1,16 +1,46 @@
 package com.tuwien.isis.irtask1;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
 /**
  * TODO
  */
 public class RankingEngine {
 
 	/**
-	 * TODO
+	 * CLI option for setting the input topics list
+	 */
+	private static final String TOPICS = "t";
+
+	/**
+	 * Handle user arguments
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO
+
+		// Create command line parser
+		Options options = new Options();
+		options.addOption(TOPICS, true, "list of input topics");
+		CommandLineParser parser = new PosixParser();
+
+		try {
+			CommandLine command = parser.parse(options, args);
+			if (command.hasOption(TOPICS)) {
+				System.out.println(command.getOptionValue(TOPICS));
+			} else {
+				throw new MissingOptionException(
+						"Topic list was not specified. Please use the -"
+								+ TOPICS + " option");
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
