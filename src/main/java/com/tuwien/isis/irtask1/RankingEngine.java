cd2682f946fcad998d2b7add44572328c211b96a
Taylor <e0725922@student.tuwien.ac.at>
2014-03-30 16:22:11 +0200

    Initial commit

---
+++ src/main/java/com/tuwien/isis/irtask1/RankingEngine.java
@@ -0,0 +1,16 @@
+package com.tuwien.isis.irtask1;
+
+/**
+ * TODO
+ */
+public class RankingEngine {
+
+	/**
+	 * TODO
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+}
